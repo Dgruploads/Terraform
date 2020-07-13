d4edcac88a33cdbf69bba610ece7209bca5d2a37
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-07-13 21:28:16 +0530

    Create main.tf

---
+++ main_variable_output/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "this" {
+  count                  = var.instance_count
+  instance_type          = var.instance_type
+  ami                    = var.image_id
+  key_name               = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+}
+
+resource "aws_security_group" "allow_http" {
+  name                   = var.name
+  description            = var.name
+  vpc_id                 = "vpc-06a5dd7cbb70bcf5e"
+
+  ingress {
+    description          = "Allow http access"
+    from_port            = 80
+    to_port              = 80
+    protocol             = "tcp"
+    cidr_blocks          = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description          = "Allow ssh access"
+    from_port            = 22
+    to_port              = 22
+    protocol             = "tcp"
+    cidr_blocks          = ["0.0.0.0/0"]
+  }
+}
