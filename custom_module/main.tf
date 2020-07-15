120ce54a57ffd89dcd26c95b8120a8a203dc323f
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-07-15 15:04:26 +0530

    Create main.tf

---
+++ custom_module/main.tf
@@ -0,0 +1,48 @@
+# Terraform configuration
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "my-ec2-cluster"
+  instance_count = 2
+
+  ami                    = "ami-0b898040803850657"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "dgruploads-terraform"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
