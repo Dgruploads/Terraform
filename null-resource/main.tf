f6e203794db019185777cc0f75ef9e00c05dae03
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-08-20 23:18:36 +0530

    Update main.tf

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -6,20 +6,12 @@ provider "aws" {
 resource "aws_instance" "cluster" {
   instance_type = "t2.micro"
   ami           = "ami-0b898040803850657"
-  count         = 4
+  count         = 2
 }
 
 resource "null_resource" "cluster" {
   # Changes to any instance of cluster will require re-provisioning.
   triggers = {
-  	cluster_instance_ids = "${join(",", aws_instance.cluster.*.id)}"
-  }
-
-connection {
-	host = "${element(aws_instance.cluster.*.public_ip, 0)}"
-}  
-
-provisioner "local-exec" {
-	command = "terraform.sh ${join(" ",aws_instance.cluster.*.private_ip)}"
+        cluster_instance_ids = "${join(",", aws_instance.cluster.*.id)}"
   }
 }
