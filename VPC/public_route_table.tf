8d7fa10404665326a25cbcf0ee92a399a9391d05
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-01 19:56:49 +0530

    Create public_route_table.tf

---
+++ VPC/public_route_table.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id      = aws_vpc.custom-vpc-terraform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom-vpc-IGW.id
+  }
+
+  tags = {
+    Name = var.public_route_table
+  }
+}
