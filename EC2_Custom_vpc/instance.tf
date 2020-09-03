40b7e4a1a831cf0fa2481ed415ba8b39692d86ee
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-03 07:14:31 +0530

    Create instance.tf

---
+++ EC2_Custom_vpc/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web_instance" {
+  ami           = lookup(var.ami_id, var.region)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.custom-vpc-public-subnet-one.id
+  vpc_security_group_ids = [aws_security_group.custom-vpc-security-group.id]
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.ec2_tags
+  }
+}
