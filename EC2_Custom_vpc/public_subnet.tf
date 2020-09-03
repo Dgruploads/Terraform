16ffb74f851b34f150978ff81d0f0127f91e6715
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-03 07:17:57 +0530

    Update public_subnet.tf

--- EC2_Custom_vpc/public_subnet.tf
+++ EC2_Custom_vpc/public_subnet.tf
@@ -10,3 +10,8 @@ resource "aws_route_table" "public_route_table" {
     Name = var.public_route_table
   }
 }
+
+resource "aws_route_table_association" "public_route_association" {
+  subnet_id   =  aws_subnet.custom-vpc-public-subnet-one.id
+  route_table_id = aws_route_table.public_route_table.id
+}
