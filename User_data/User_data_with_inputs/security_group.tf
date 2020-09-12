0b83e22082650665d164c007eb108ced6b4b68ef
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-12 11:10:46 +0530

    Create security_group.tf

---
+++ User_data/User_data_with_inputs/security_group.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "custom_security_group" {
+  name          = "custom-vpc-security-group"
+  description   = "custom-vpc-security-group"
+
+  ingress {
+    description = "custom-vpc-security-group"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Custom security group"
+  }
+}
