64a4d0f1298f43b2add8b6ae2674904048d22cbf
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-08-17 08:03:42 +0530

    Update main.tf

--- Remote_exec/main.tf
+++ Remote_exec/main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  profile = "default"
+  region  = var.RegionName
+}
 
+resource "aws_instance" "cluster" {
+  instance_type = var.InstanceType
+  ami           = var.ImageAmi
+  key_name	    = var.key_name
+}
+
+resource "null_resource" "remote_exec_demo" {
+  depends_on = ["aws_instance.cluster"]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = "${file("/home/ec2-user/remote_exec/linux-server-mac.pem")}"
+    host = "${aws_instance.cluster.public_ip}"
+    port = 22
+  }
+
+  provisioner "file" {
+    source	= "script.sh"
+    destination = "/home/ec2-user/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/script.sh",
+      "/home/ec2-user/script.sh"
+    ]
+  }
+}
