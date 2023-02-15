7318bfced4baeef74d5472f325602b23e88db84c
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-02-15 20:51:53 +0530

    Create variables.tf

---
+++ Data sources/ec2_subnet_key/variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_instance_type" {
+  type = string
+  description = "Instance type to use when launching the instance"
+}
+
+variable "ec2_ami" {
+  type = string
+  description = "AMI to use when launching the instance"
+}
+
+variable "ec2_key_name" {
+  type = string
+  description = "Key name to use when launching the instance"
+}
