282c0148f8d6127528d9eeedc89f82a7d00b0ce2
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-02-19 08:57:36 +0530

    Update main.tf

--- provisioners/file/main.tf
+++ provisioners/file/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web_instance" {
   instance_type = var.ec2_instance_type
   ami           = var.ec2_ami
+  key_name      = "${data.aws_key_pair.existing_key_pair.key_name}"
 
   provisioner "file" {
     source      = "script.sh"
@@ -8,7 +9,7 @@ resource "aws_instance" "web_instance" {
 
     connection {
       type = "ssh"
-      user = "ec2-user"
+      user = "ubuntu"
       private_key = "${file("/home/ubuntu/remote_exec/linux_key_1.pem")}"
       host = "${aws_instance.web_instance.public_ip}"
       port = 22
