915f237432ca92209b90882849c182b541d92e29
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-02-19 17:09:36 +0530

    Delete Remote_exec directory

--- Remote_exec/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.cluster.public_ip
-}
--- Remote_exec/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo yum install httpd -y
-echo "This is running on infara provisioned by terraform " > /var/www/html/index.html
-sudo service httpd start
