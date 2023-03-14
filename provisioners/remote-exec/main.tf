92a93467e7349426dd456f3900bcd0fa98e7a820
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-14 07:30:17 +0530

    Update main.tf

--- provisioners/remote-exec/main.tf
+++ provisioners/remote-exec/main.tf
@@ -2,11 +2,12 @@ resource "aws_instance" "example_instance" {
   instance_type = var.ec2_instance_type
   ami           = var.ec2_ami
   key_name      = var.ec2_key_name
+  tags          = local.common_tags
 
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = "${file("/home/ubuntu/remote_exec/linux_key_1.pem")}"
+    private_key = "${file("/home/ubuntu/remote_exec/new_key.pem")}"
     host = "${aws_instance.example_instance.public_ip}"
     port = 22
   }
