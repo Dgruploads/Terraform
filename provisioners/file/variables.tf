fd04e5f260561acd60bf0d93ebeba4d0c96cfd79
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-14 07:27:21 +0530

    Update variables.tf

--- provisioners/file/variables.tf
+++ provisioners/file/variables.tf
@@ -1,14 +1,19 @@
 variable "ec2_key_name" {
-  type = string
   description = "Key name to use when launching the instance"
+  type        = string
 }
 
 variable  "ec2_instance_type" {
-  type = string
   description = "Instance type to use when launching the instance"
+  type        = string
 }
 
 variable "ec2_ami" {
-  type = string
   description = "AMI to use when launching the instance"
+  type        = string
+}
+
+variable "ec2_region" {
+  description = "Specify the region to create the resources"
+  type        = string
 }
