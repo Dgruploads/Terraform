5641449ffeb3b436048949c9e64956d0e5c3a6ef
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-15 17:52:14 +0530

    Delete variables.tf

--- meta_arguments/for_each/variables.tf
+++
@@ -1,7 +0,0 @@
-aws_region  = "us-east-1"
-image_id    = { 
-  image_one   = "ami-0557a15b87f6559cf" # Ubuntu AMI
-  image_two   = "ami-005f9685cb30f234b" # Amazon linux AMI
-  image_three = "ami-0c2b0d3fb02824d92" # Windows AMI
-}
-instance_type = "t2.micro"
