9616fdc3e5de7cb64ef945d270ada55c8af74ebe
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-15 18:07:30 +0530

    Create variables.tf

---
+++ meta_arguments/depends_on/variables.tf
@@ -0,0 +1,49 @@
+variable "ec2_instance_type" {
+  description = "specify the instance type for EC2"
+  type        = string
+}
+
+variable "ec2_image" {
+  description = "specify the AMI for EC2"
+  type        = string
+}
+
+variable "ec2_region" {
+  description = "Specify the region for resources"
+  type        = string
+}
+
+variable "db_engine" {
+  description = "Specify the database engine"
+  type        = string
+}
+
+variable "db_instance_type" {
+  description = "Specify the instance type for database"
+  type        = string
+}
+
+variable "db_storage" {
+  description = "Specify the storage for the database"
+  type        = number
+}
+
+variable "db_name" {
+  description = "Specify the name for the database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Specify the username for the database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Specify the password for the database"
+  type        = string
+}
+
+variable "skip_snapshot" {
+  description = "Specify if to skip snapshot creation"
+  type        = bool
+}
