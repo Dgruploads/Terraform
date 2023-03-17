af74bb6c34f5250fdfe3ba1f0438f0510be50470
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-17 07:43:05 +0530

    Create main.tf

---
+++ meta_arguments/lifecycle/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "lifecycle_example_one" {
+  ami           = var.ec2_image_one
+  instance_type = var.ec2_instance_type
+  
+  tags          = local.common_tags
+  
+  lifecycle {
+    create_before_destroy = true
+}
