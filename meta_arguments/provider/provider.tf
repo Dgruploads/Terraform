8b9318cd5c946c5f2e8d03e18fb73038b1331868
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-03-25 08:19:19 +0530

    Update provider.tf

--- meta_arguments/provider/provider.tf
+++ meta_arguments/provider/provider.tf
@@ -1,7 +1,9 @@
+# Default proivider configuration
 provider "aws" {
   region  = var.ec2_region
 }
 
+# Alternate provider configuration
 provider "aws" {
   alias  = "Oregon"
   region = "us-west-2"
