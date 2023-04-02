a2c69a911ec5c3cbcd5405107575ad78fa20bcfd
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-04-02 15:11:35 +0530

    Create main.tf

---
+++ modules/github_module/main.tf
@@ -0,0 +1,8 @@
+module "child_module_example" {
+  source                = "github.com/Dgruploads/Terraform/modules/root_module"
+  ec2_image             = var.ec2_image
+  ec2_instance_type     = var.ec2_instance_type
+  ec2_vpc_id            = var.ec2_vpc_id
+  s3_bucket_name        = var.s3_bucket_name  
+  ec2_region            = var.ec2_region 
+}
