d154e1bba5b6359d6cb8ace4108ff9959e74e80f
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2023-04-02 07:44:40 +0530

    Update main.tf

--- modules/child_module/main.tf
+++ modules/child_module/main.tf
@@ -1,5 +1,5 @@
 module "child_module_example" {
-  source                = "/home/ubuntu/terraform/modules/root_module"
+  source                = "../root_module"
   ec2_image             = var.ec2_image
   ec2_instance_type     = var.ec2_instance_type
   ec2_vpc_id            = var.ec2_vpc_id
